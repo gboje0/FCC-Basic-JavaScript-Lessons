4cb103fc450e2f689c60429dabb1ad0fdc2d3e54
Roger K <user@misterhtmlcss.local>
2016-08-05 22:50:24 -0600

    FCC Basic JavaScript Lessons
    First commit on these related exercises

---
+++ comparison_with_equality_operator.js
@@ -0,0 +1,22 @@
+// I use console log instead of return, so sometimes that has an impact on how the less was done on my computer
+
+function testEqual(val) {
+    if(val == 12) {
+     console.log ("Equal");
+ } else {
+     console.log ("Not Equal");
+ }
+     
+}
+testEqual(12);
+
+
+// function testIfEqual(whatnumbershallwetest) {
+//     if(whatnumbershallwetest > 32) {
+//         return "that is not a good year!";
+//     } else {
+//         return "I'm soooo soo young still!!!";
+//     }
+// }
+
+// console.log(testIfEqual(31));
---
+++ comparison_with_the_strict_equality_operator.js
@@ -0,0 +1,10 @@
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    console.log ("Equal");
+  } else {
+  console.log ("Not Equal");
+  }
+}
+
+// Change this value to test
+testStrict(7);
