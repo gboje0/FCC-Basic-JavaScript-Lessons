baf35f89fb53ed45b034b85cc688b2b598bee34f
misterhtmlcss <misterhtmlcss@gmail.com>
2016-09-05 14:02:50 -0600

    more fcc basic js

---
+++ iterate_with_javascript_for_loops.js
@@ -0,0 +1,29 @@
+// Instructions
+// Use a for loop to work to push the values 1 through 5 onto myArray.
+
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i < 6; i++) {
+    myArray.push(i);
+}
+
+console.log(myArray);   
+
+// Second try 
+// It will go through a phone number sequence
+
+var secondArray;
+for(i = 0; i <= 7; i++) {
+    if(i = 0)
+    
+}
